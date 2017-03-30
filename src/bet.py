e13abb53e4949e2115fcd729a544772657bf5d2d
Giuseppe Afe <aquila941@hotmail.it>
2017-03-30 15:26:39 +0100

    Added colors

--- src/bet.py
+++ src/bet.py
@@ -1,3 +1,5 @@
+import bcolors
+
 class Bet(object):
 
   def __init__(self, odds, match):
@@ -27,8 +29,8 @@ class Bet(object):
 
   def __str__(self):
     if self.expectedResult != self.match.getResult():
-      return "{0:<20}\t{1:<20}\t{2:>5}\t\t{3:>3}\t\t{4:.2f}\t{5:>6.2f}\t{6:>10.2f}\t{7:>10.2f}".format(self.match.homeTeam, self.match.awayTeam, self.expectedResult, self.match.getResult(), self.odds, self.fraction, self.accountBefore, self.accountAfter)
-    return "{0:<20}\t{1:<20}\t\t\t\t\t{2:.2f}\t{3:>6.2f}\t{4:>10.2f}\t{5:>10.2f}".format(self.match.homeTeam, self.match.awayTeam, self.odds, self.fraction, self.accountBefore, self.accountAfter)
+      return "\033[91m{0:<20}\t{1:<20}\t{2:>5}\t\t{3:>3}\t\t{4:.2f}\t{5:>6.2f}\t{6:>10.2f}\t{7:>10.2f}\033[0m".format(self.match.homeTeam, self.match.awayTeam, self.expectedResult, self.match.getResult(), self.odds, self.fraction, self.accountBefore, self.accountAfter)
+    return "\033[92m{0:<20}\t{1:<20}\t\t\t\t\t{2:.2f}\t{3:>6.2f}\t{4:>10.2f}\t{5:>10.2f}\033[0m".format(self.match.homeTeam, self.match.awayTeam, self.odds, self.fraction, self.accountBefore, self.accountAfter)
     
   def __repr__(self):
     return self.__str__()
